d23cff7034ee89c103bc61370f5b8324555e0659
esthergithub <esther.mugo@africascan.com>
2021-07-12 18:35:09 +0200

    design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,4 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -11,12 +8,15 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
-// Read Sprockets README (https://github.com/rails/XXXXXXXXXXXXXXXXXXXXXX) for details
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
 //= require rails-ujs
+//= require activestorage
 //= require turbolinks
 //= require jquery/dist/jquery.js
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
 
 import Rails from "@rails/ujs"
