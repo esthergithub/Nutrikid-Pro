e8b27144c80f3b535643856ee4b5d04fc9e861cd
PeterYves <iradupeteryves@gmail.com>
2021-07-13 19:26:13 +0200

    finalizing comment function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_110120) do
+ActiveRecord::Schema.define(version: 2021_07_13_144140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,9 @@ ActiveRecord::Schema.define(version: 2021_07_13_110120) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["patient_id"], name: "index_comments_on_patient_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "patients", force: :cascade do |t|
@@ -97,6 +99,7 @@ ActiveRecord::Schema.define(version: 2021_07_13_110120) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "patients"
+  add_foreign_key "comments", "users"
   add_foreign_key "patients", "categories"
   add_foreign_key "patients", "users"
 end
